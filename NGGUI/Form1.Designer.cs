b656267887df6aad05c0e47be024f500e4f11830
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-10-05 09:57:22 +0200

    ostali fajlovi

--- NGGUI/Form1.Designer.cs
+++ NGGUI/Form1.Designer.cs
@@ -1,11 +1,17 @@
-﻿namespace NGGUI
+﻿using System;
+
+namespace NGGUI
 {
-    partial class Form1
+    partial class Numbers_Game
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+
+
+
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,13 +34,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxSolution = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectedNumber1 = new System.Windows.Forms.TextBox();
+            this.selectedNumber2 = new System.Windows.Forms.TextBox();
+            this.selectedNumber4 = new System.Windows.Forms.TextBox();
+            this.selectedNumber3 = new System.Windows.Forms.TextBox();
+            this.selectedNumber6 = new System.Windows.Forms.TextBox();
+            this.selectedNumber5 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.ResultLable = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxSolution
+            // 
+            this.textBoxSolution.Location = new System.Drawing.Point(91, 281);
+            this.textBoxSolution.Name = "textBoxSolution";
+            this.textBoxSolution.Size = new System.Drawing.Size(603, 22);
+            this.textBoxSolution.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your solution:";
+            // 
+            // selectedNumber1
+            // 
+            this.selectedNumber1.Location = new System.Drawing.Point(91, 141);
+            this.selectedNumber1.MinimumSize = new System.Drawing.Size(50, 50);
+            this.selectedNumber1.Name = "selectedNumber1";
+            this.selectedNumber1.ReadOnly = true;
+            this.selectedNumber1.Size = new System.Drawing.Size(50, 22);
+            this.selectedNumber1.TabIndex = 2;
+            // 
+            // selectedNumber2
+            // 
+            this.selectedNumber2.Location = new System.Drawing.Point(191, 141);
+            this.selectedNumber2.MinimumSize = new System.Drawing.Size(50, 50);
+            this.selectedNumber2.Name = "selectedNumber2";
+            this.selectedNumber2.ReadOnly = true;
+            this.selectedNumber2.Size = new System.Drawing.Size(50, 22);
+            this.selectedNumber2.TabIndex = 3;
+            // 
+            // selectedNumber4
+            // 
+            this.selectedNumber4.Location = new System.Drawing.Point(421, 141);
+            this.selectedNumber4.MinimumSize = new System.Drawing.Size(50, 50);
+            this.selectedNumber4.Name = "selectedNumber4";
+            this.selectedNumber4.ReadOnly = true;
+            this.selectedNumber4.Size = new System.Drawing.Size(50, 22);
+            this.selectedNumber4.TabIndex = 5;
+            // 
+            // selectedNumber3
+            // 
+            this.selectedNumber3.Location = new System.Drawing.Point(304, 141);
+            this.selectedNumber3.MinimumSize = new System.Drawing.Size(50, 50);
+            this.selectedNumber3.Name = "selectedNumber3";
+            this.selectedNumber3.ReadOnly = true;
+            this.selectedNumber3.Size = new System.Drawing.Size(50, 22);
+            this.selectedNumber3.TabIndex = 4;
+            // 
+            // selectedNumber6
+            // 
+            this.selectedNumber6.Location = new System.Drawing.Point(644, 141);
+            this.selectedNumber6.MinimumSize = new System.Drawing.Size(50, 50);
+            this.selectedNumber6.Name = "selectedNumber6";
+            this.selectedNumber6.ReadOnly = true;
+            this.selectedNumber6.Size = new System.Drawing.Size(50, 22);
+            this.selectedNumber6.TabIndex = 7;
+            // 
+            // selectedNumber5
+            // 
+            this.selectedNumber5.Location = new System.Drawing.Point(529, 141);
+            this.selectedNumber5.MinimumSize = new System.Drawing.Size(50, 50);
+            this.selectedNumber5.Name = "selectedNumber5";
+            this.selectedNumber5.ReadOnly = true;
+            this.selectedNumber5.Size = new System.Drawing.Size(50, 22);
+            this.selectedNumber5.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(344, 52);
+            this.textBox8.MinimumSize = new System.Drawing.Size(100, 50);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 8;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(713, 280);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 9;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(23, 21);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(105, 23);
+            this.newGameButton.TabIndex = 10;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameBtn_Click);
+            // 
+            // ResultLable
+            // 
+            this.ResultLable.AutoSize = true;
+            this.ResultLable.Location = new System.Drawing.Point(361, 361);
+            this.ResultLable.Name = "ResultLable";
+            this.ResultLable.Size = new System.Drawing.Size(48, 17);
+            this.ResultLable.TabIndex = 11;
+            this.ResultLable.Text = "Result";
+            this.ResultLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ResultLable.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(144, 22);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown_SelectBig);
+            // 
+            // Numbers_Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.ResultLable);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.selectedNumber6);
+            this.Controls.Add(this.selectedNumber5);
+            this.Controls.Add(this.selectedNumber4);
+            this.Controls.Add(this.selectedNumber3);
+            this.Controls.Add(this.selectedNumber2);
+            this.Controls.Add(this.selectedNumber1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSolution);
+            this.Name = "Numbers_Game";
+            this.Text = "Numbers Game";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
-    }
+
+        private System.Windows.Forms.TextBox textBoxSolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox selectedNumber1;
+        private System.Windows.Forms.TextBox selectedNumber2;
+        private System.Windows.Forms.TextBox selectedNumber4;
+        private System.Windows.Forms.TextBox selectedNumber3;
+        private System.Windows.Forms.TextBox selectedNumber6;
+        private System.Windows.Forms.TextBox selectedNumber5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label ResultLable;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }    
 }
 
--- NGGUI/Form1.cs
+++ NGGUI/Form1.cs
@@ -10,11 +10,297 @@ using System.Windows.Forms;
 
 namespace NGGUI
 {
-    public partial class Form1 : Form
+    public partial class Numbers_Game : Form
     {
-        public Form1()
+        
+        private int[] selectedNumbers = new int[6];
+        private int goal;
+        private string solution;
+        private System.Random rnd = new System.Random();
+        private int result;
+        int big;
+
+
+        public Numbers_Game()
         {
             InitializeComponent();
+            Game(2);
         }
+
+        void newGameBtn_Click(Object sender,
+                           EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            textBoxSolution.Clear();
+            Game(big);
+        }
+
+        void submitBtn_Click(Object sender,
+                           EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            solution = textBoxSolution.Text;
+            if (!ValidSolution(solution))
+            {
+                ResultLable.Text = "Invalid solution";
+                return;
+            }
+            result = Calculate(solution);
+
+            ResultLable.Visible = true;
+            ResultLable.Text = CalculateGameResult();
+
+        }
+
+        void numericUpDown_SelectBig(Object sender,
+                           EventArgs e)
+        {
+            big = (int)numericUpDown1.Value;
+        }
+
+
+
+
+        private void Game(int big)
+        {
+
+            System.Windows.Forms.TextBox[] textBoxes = { selectedNumber1, selectedNumber2, selectedNumber3, selectedNumber4, selectedNumber5, selectedNumber6 };
+            
+            goal = rnd.Next(100, 1000);
+            textBox8.Text = goal.ToString();
+
+            SelectNumbers(big);//promeni kasnije
+
+            int i = 0;
+            foreach (System.Windows.Forms.TextBox tb in textBoxes)
+            {
+                tb.Text = selectedNumbers[i].ToString();
+                tb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+                i++;
+            }
+
+            ResultLable.Visible = false;
+            
+        }
+
+        public bool SelectNumbers(int big)
+        {
+            if (big > 4)
+            {
+                return false;
+            }
+            int small = 6 - big;
+            for (int i = 0; i < small; i++)
+            {
+                selectedNumbers[i] = rnd.Next(1, 10);
+                int exists = 0;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (i != 0 && selectedNumbers[i] == selectedNumbers[j])
+                        exists++;
+                }
+                if (exists >= 2)
+                {
+                    selectedNumbers[i] = 0;
+                    i--;
+                }
+
+            }
+            if (big == 4)
+            {
+                for (int i = 2; i < 6; i++)
+                {
+                    selectedNumbers[i] = 25 * (i - 1);
+                }
+            }
+            else if (big == 3)
+            {
+                int notSelected = rnd.Next(1, 5);
+                switch (notSelected)
+                {
+                    case 1:
+                        selectedNumbers[3] = 50;
+                        selectedNumbers[4] = 75;
+                        selectedNumbers[5] = 100;
+                        break;
+                    case 2:
+                        selectedNumbers[3] = 25;
+                        selectedNumbers[4] = 75;
+                        selectedNumbers[5] = 100;
+                        break;
+                    case 3:
+                        selectedNumbers[3] = 25;
+                        selectedNumbers[4] = 50;
+                        selectedNumbers[5] = 100;
+                        break;
+                    case 4:
+                        selectedNumbers[3] = 25;
+                        selectedNumbers[4] = 50;
+                        selectedNumbers[5] = 75;
+                        break;
+                    default:
+                        selectedNumbers[3] = 25;
+                        selectedNumbers[4] = 50;
+                        selectedNumbers[5] = 100;
+                        break;
+
+                }
+            }
+            else
+            {
+                for (int i = small; i < big + small; i++)
+                {
+                    int selected = rnd.Next(1, 5);
+                    switch (selected)
+                    {
+                        case 1:
+                            selectedNumbers[i] = 25;
+                            break;
+                        case 2:
+                            selectedNumbers[i] = 50;
+                            break;
+                        case 3:
+                            selectedNumbers[i] = 75;
+                            break;
+                        case 4:
+                            selectedNumbers[i] = 100;
+                            break;
+                        default:
+                            i--;
+                            break;
+                    }
+                    if (big == 2 && i != small && selectedNumbers[i] == selectedNumbers[i - 1]) i--;
+                }
+            }
+            return true;
+        }
+
+        public string RemoveBrackets(ref string solution, ref int i)
+        {
+            string t = ""; i++;
+            while (solution[i] != ')')
+            {
+                if (solution[i] == '(')
+                {
+                    RemoveBrackets(ref solution, ref i);
+                    return solution;
+                }
+                t += solution[i];
+                i++;
+            }
+            solution = solution.Replace('(' + t + ')', Calculate(t).ToString());
+            i = -1;
+            return solution;
+        }
+
+        public int Calculate(string solution)
+        {
+            int l = 0, r = 0;
+
+            if (!(solution.Contains("(") || solution.Contains("-") || solution.Contains("+") ||
+                solution.Contains("/") || solution.Contains("*"))) return Int32.Parse(solution);
+
+            for (int i = 0; i < solution.Length; i++)
+            {
+                if (solution[i] == '(')
+                {
+                    solution = RemoveBrackets(ref solution, ref i);
+                    if (!(solution.Contains("(") || solution.Contains("-") || solution.Contains("+") ||
+                    solution.Contains("/") || solution.Contains("*"))) return Int32.Parse(solution);
+                    continue;
+                }
+                if (!"+-/*".Contains(solution[i].ToString())) continue;
+                switch (solution[i])
+                {
+                    case '+':
+                        l = Calculate(solution.Remove(i));
+                        r = Calculate(solution.Remove(0, i + 1));
+                        return l + r;
+                    case '-':
+                        if (solution.Remove(0, i + 1).Contains("-") || solution.Remove(0, i + 1).Contains("+")) continue;
+                        l = Calculate(solution.Remove(i));
+                        r = Calculate(solution.Remove(0, i + 1));
+                        return l - r;
+                    case '*':
+                        if (solution.Remove(0, i).Contains("+") || solution.Remove(0, i).Contains("-")) continue;
+                        l = Calculate(solution.Remove(i));
+                        r = Calculate(solution.Remove(0, i + 1));
+                        return l * r;
+                    case '/':
+                        if (solution.Remove(0, i).Contains("+") || solution.Remove(0, i).Contains("-")) continue;
+                        l = Calculate(solution.Remove(i));
+                        r = Calculate(solution.Remove(0, i + 1));
+                        return l / r;
+                    default:
+                        continue;
+                }
+            }
+            return 0;
+        }
+
+        public bool ValidSolution(string solution)
+        {
+            if (!BracketsCorrect(solution)) return false;
+
+            foreach (char c in solution)
+                if (!"0123456789+-/*()".Contains(c.ToString())) return false;
+
+
+
+            string[] t = solution.Split(new char[] { '/', '*', '+', '-', ')', '(' }, StringSplitOptions.RemoveEmptyEntries);
+            bool acp = true;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                for (int j = 0; j < selectedNumbers.Length; j++)
+                {
+                    if (System.Int32.Parse(t[i]) == selectedNumbers[j])
+                    {
+                        selectedNumbers[j] = 0;
+                        acp = true;
+                        break;
+                    }
+                    else acp = false;
+                }
+            }
+            return acp;
+        }
+
+        public static bool BracketsCorrect(string s)
+        {
+            int nrBrackets = 0;
+            foreach (char c in s)
+            {
+                if (c == '(') nrBrackets++;
+                if (c == ')') nrBrackets--;
+                if (nrBrackets < 0) return false;
+
+            }
+            if (nrBrackets == 0) return true;
+            return false;
+        }
+
+        public string CalculateGameResult()
+        {
+            switch (Math.Abs(goal - result))
+            {
+                case 0:
+                    return $"{ result }\nYou've scored 10 points!";
+                case 1:
+                    return $"{ result }\nYou've scored 7 points!";
+                case 2:
+                    return $"{ result }\nYou've scored 5 points!";
+                case 3:
+                    return $"{ result }\nYou've scored 3 points.";
+                default:
+                    return  $"{ result }\nSorry, not close enough to the goal. You've scored 0 points.";
+            }
+        }
+
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button newGameButton;
     }
+
 }
+
